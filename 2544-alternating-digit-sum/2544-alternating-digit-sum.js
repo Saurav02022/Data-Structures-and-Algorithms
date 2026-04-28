988aa8b772648326f9c530c404fdc43b3b12b1c8
SAURAV KUMAR <104342116+Saurav02022@users.noreply.github.com>
2026-04-28 20:43:29 +0530

    Time: 0 ms (100%), Space: 53.9 MB (28.83%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var alternateDigitSum = function (n) {
+    let digits = String(n).split("");
+    let symbol = 1;   // +1 means plus, -1 means minus
+    let sum = 0;
+
+    for (let i = 0; i < digits.length; i++) {
+        sum += Number(digits[i]) * symbol;
+        symbol *= -1; // flip sign
+    }
+
+    return sum;
+
+};
