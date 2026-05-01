5f64ff8670e3d3e0140675b254d4174247e9b3e8
SAURAV KUMAR <104342116+Saurav02022@users.noreply.github.com>
2026-05-01 20:02:04 +0530

    Time: 0 ms (100%), Space: 54.9 MB (15.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,20 +3,27 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    for (let i = 0; i <= nums.length - 2; i++) {
-        let minIndex = i;
+    let count = {
+        0: 0,
+        1: 0,
+        2: 0
+    };
 
-        for (let j = i + 1; j <= nums.length - 1; j++) {
-            if (nums[j] < nums[minIndex]) {
-                minIndex = j;
-            }
-        }
+    // Count frequency of 0, 1, and 2
+    for (let i = 0; i < nums.length; i++) {
+        count[nums[i]]++;
+    }
 
-        if (minIndex !== i) {
-            let temp = nums[minIndex];
-            nums[minIndex] = nums[i];
-            nums[i] = temp;
+    // Rewrite nums in sorted order
+    let index = 0;
+
+    for (let color = 0; color <= 2; color++) {
+        let freq = count[color];
+
+        for (let j = 0; j < freq; j++) {
+            nums[index] = color;
+            index++;
         }
     }
-    return nums;
+    return nums
 };
